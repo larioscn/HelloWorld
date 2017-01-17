48ebc2a0562fce4185689ceb4ac1de839a6d9562
larioscn <larios.cn@knights.ucf.edu>
2017-01-17 14:05:30 -0500

    HelloWorld First program yay!

---
+++ test.cpp
@@ -0,0 +1,15 @@
+// my first program in C++
+#include <iostream>
+
+int main()
+{
+	int c = 9;
+	std::cout << c << std::endl;
+	c = 4;
+	std::cout << c << std::endl;
+	c = 29;
+	std::cout << c << std::endl;
+
+
+  std::cout << "Hello World!" << std::endl;
+}
